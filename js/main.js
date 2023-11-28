06b091531f4ce8f1cdd2548c7afa453963f309d9
ahmedmansour779 <ahmedmansour1239@gmail.com>
2023-11-28 11:50:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,210 @@
+const searchInput = document.getElementById("search-input")
+const backgroundSidebar = document.getElementById("backgroundSidebar")
+
+// console.log(searchInput);
+
+function show() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+function toggleNavbar() {
+  var sidebar = document.getElementById("navbar");
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "250px";
+  }
+}
+// add newInput
+function toggleVisibility() {
+  let element = document.getElementById("newInput");
+  if (element.classList.contains("d-none")) {
+    element.classList.remove("d-none");
+    element.classList.add("d-block");
+  } else {
+    element.classList.remove("d-block");
+    element.classList.add("d-none");
+  }
+}
+function toggleInVisibility() {
+  let element = document.getElementById("newInput");
+  if (element.classList.contains("d-block")) {
+    element.classList.remove("d-block");
+    element.classList.add("d-none");
+  } else {
+    element.classList.remove("d-none");
+    element.classList.add("d-block");
+  }
+}
+//   sideBar
+let sidebar = document.querySelector(".sideBar");
+let content = document.querySelector(".content");
+// let meals = document.getElementById("meals");
+
+function handleBtnClose() {
+  sidebar.style.right = "-256px";
+  console.log("test");
+  hiddenBackgroundSidebar()
+}
+
+if (sidebar) {
+  sidebar.style.right = "-256px";
+  content.style.marginRight = "0";
+  // meals.style.marginRight = "0";
+}
+
+function showBackgroundSidebar() {
+  backgroundSidebar.style.display = "block"
+}
+
+function hiddenBackgroundSidebar() {
+  backgroundSidebar.style.display = "none"
+}
+
+function toggleSidebar() {
+  // let sidebar = document.querySelector(".sideBar");
+  // let content = document.querySelector(".content");
+  // let meals = document.getElementById("meals");
+
+  if (sidebar.style.right === "-256px") {
+    sidebar.style.right = "-17px";
+    // content.style.marginRight = "256px";
+    // meals.style.marginRight = "256px";
+  } else {
+    sidebar.style.right = "-256px";
+    content.style.marginRight = "0";
+    // meals.style.marginRight = "0";
+  }
+  showBackgroundSidebar()
+  // backgroundSidebar.style.display = "block"
+}
+//   counter
+let counters = {};
+
+function updateCounter(cardId) {
+  document
+    .getElementById(cardId)
+    .getElementsByClassName("counter")[0].innerText = counters[cardId];
+}
+
+function increment(cardId) {
+  if (!counters[cardId]) {
+    counters[cardId] = 0;
+  }
+  counters[cardId]++;
+  updateCounter(cardId);
+}
+
+function decrement(cardId) {
+  if (counters[cardId] && counters[cardId] > 0) {
+    counters[cardId]--;
+    updateCounter(cardId);
+  }
+}
+
+//Dropdown
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+// =-=-=-=-=-=-==-=-=-==========Dark Mood===============
+
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.body;
+  const toggleBtn = document.getElementById("toggleBtn");
+
+  if (localStorage.getItem("dark") === "enabled") {
+    enableDarkMode();
+  }
+
+  toggleBtn.addEventListener("click", function () {
+    if (body.classList.contains("dark")) {
+      // Disable dark mode
+      disableDarkMode();
+    } else {
+      // Enable dark mode
+      enableDarkMode();
+    }
+  });
+
+  function enableDarkMode() {
+    body.classList.add("dark");
+    localStorage.setItem("darkMode", "enabled");
+  }
+
+  function disableDarkMode() {
+    body.classList.remove("dark");
+    localStorage.setItem("darkMode", null);
+  }
+});
+var activeButton = document.getElementById("activeButton");
+var activeButtonWomen = document.getElementById("activeButtonWomen");
+// search side
+const searchContainer = document.getElementById("search-container");
+searchContainer.style.display = "block";
+
+function toggleInputSearch() {
+  searchInput.style.display == "none" ?
+    searchInput.style.display = "block" :
+    searchInput.style.display = "none"
+}
+
+function toggleSearchContainer() {
+  toggleInputSearch()
+  searchContainer.style.display =
+    (searchContainer.style.display === "none" ||
+      searchContainer.style.display === "")
+      ? "block"
+      : "none";
+}
+
+activeButton.addEventListener("click", function () {
+  activeButton.classList.add("activeButton");
+  activeButtonWomen.classList.remove("activeButton");
+});
+
+activeButtonWomen.addEventListener("click", function () {
+  activeButton.classList.remove("activeButton");
+
+  activeButtonWomen.classList.add("activeButton");
+});
+
+
+
+let x = document.getElementById("closeForm");
+
+document.getElementById("myButton").onclick = function () {
+  alert("Button clicked!");
+};
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("notificationDropdown");
+  dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+}
+
+//=======================upload image=================
+function previewImage(input) {
+  const file = input.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      document.getElementById("profile-image").src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
+
+// hide side bar 
